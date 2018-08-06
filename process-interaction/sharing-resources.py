5832b9f2aefbe3e2ecca8a49f3bd02e8e5dccd82
Tolga Karahan <tkarahan147@gmail.com>
2018-08-06 15:46:01 +0300

    Update sharing-resources.py

--- process-interaction/sharing-resources.py
+++ process-interaction/sharing-resources.py
@@ -1,23 +1,27 @@
 import simpy
 
+# Elektrikli arac orneginden yola cikarak araclarin sarj olmak icin bir istasyona
+# gittigini ve iki adet olan slotlari paylastigini dusunelim
+
 def car(env, name, bcs, driving_time, charge_duration):
-	# Simulate driving to the BCS
+	# Aracin istasyona gitmesi
 	yield env.timeout(driving_time)
 
-	# Request one of its charging spots
+	# Kaynaklardan(sarj slotu) birine istekte bulunmasi
 	print('%s arriving at %d' % (name, env.now))
 	with bcs.request() as req:
 		yield req
 
-		# Charge the battery
+		# Sarj edilmesi
 		print('%s starting to charge at %s' % (name, env.now))
 		yield env.timeout(charge_duration)
 		print('%s leaving the bcs at %s' % (name, env.now))
 
-
+# Ortam ve paylasilacak kaynagin uretilmesi
 env = simpy.Environment()
 bcs = simpy.Resource(env, capacity=2)
 
+# Elektrikli araclarin davranisini simule eden processler
 for i in range(4):
 	env.process(car(env, 'car %d' % i, bcs, i*2, 5))
 
